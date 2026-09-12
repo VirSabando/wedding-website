e3b5335b4fb9c7e9c83637de2b92797dad85407d
VirSabando <virsabando@gmail.com>
2026-09-12 16:48:44 +0000

    contraste

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import FoliageAmbient from './FoliageAmbient.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
       <App />
-      <div id="celebrate" className="celebrate-root" />
+      <FoliageAmbient />
     </>
   </StrictMode>,
 )
